6a813ef177cbe3d5c5b35d58bb17b290c8fbf02b
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-08-14 14:27:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// This script is not required for the animation, but allows for speed control (optional)
+
+const earth = document.querySelector('.earth');
+let speed = 5; // Adjust the number (in seconds) to control rotation speed
+
+earth.style.animationDuration = `${speed}s`;
